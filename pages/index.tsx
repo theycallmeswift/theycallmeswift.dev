8793d4114a3ff14d70c4fc4d074e75380097d430
Swift <theycallmeswift@gmail.com>
2022-08-08 22:33:01 -0400

    Add welcome message to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
 import type { NextPage } from "next";
 
 import DefaultLayout from "layouts/DefaultLayout";
+import Image from "next/future/image";
+import Link from "components/Link";
 
 const Home: NextPage = () => {
   return (
     <DefaultLayout>
-      <h1>Hello World</h1>
+      <div className="flex flex-col-reverse sm:flex-row items-start w-full justify-between">
+        <div className="flex flex-col sm:pr-8">
+          <h1 className="text-4xl sm:text-2xl font-bold mb-2">
+            Hi there, you can call me Swift.
+          </h1>
+          <p className="">
+            I&apos;m the CEO & Co-Founder of{" "}
+            <Link href="http://mlh.io/">Major League Hacking (MLH)</Link> where
+            I&apos;m helping the next generation of developers launch their
+            careers.
+          </p>
+        </div>
+        <Image
+          alt="Mike Swift"
+          height={175}
+          width={175}
+          sizes="20vw"
+          src="/avatar.jpg"
+          className="rounded-lg mb-4 sm:mb-0"
+        />
+      </div>
     </DefaultLayout>
   );
 };
