11cab7fc349e5db4f4430f09ac6a0b14166d3c39
Swift <theycallmeswift@gmail.com>
2022-08-11 15:07:45 -0400

    Add animated glider favicon & logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,9 @@ import type { LinkProps } from "components/Link";
 
 import clsx from "clsx";
 import Link from "components/Link";
+import Image from "next/future/image";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const NavLink = ({ href, ...props }: LinkProps) => {
   const router = useRouter();
@@ -14,10 +16,43 @@ const NavLink = ({ href, ...props }: LinkProps) => {
   return <Link href={href} className={rootClassName} {...props} />;
 };
 
-const Footer: React.FC = () => {
+const Navbar: React.FC = () => {
+  const [isHovering, setIsHovered] = useState(true);
+  const onMouseEnter = () => setIsHovered(true);
+  const onMouseLeave = () => setIsHovered(false);
+
+  useEffect(() => {
+    const logoTimer = setTimeout(() => setIsHovered(false), 2000);
+
+    return () => {
+      clearTimeout(logoTimer);
+    };
+  });
+
   return (
     <nav className="flex items-center justify-between w-full mx-auto pt-8 pb-16">
-      <div>
+      <div className="flex items-end">
+        <Link href="/" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+          {isHovering ? (
+            <Image
+              alt="Glider Logo"
+              height={20}
+              width={20}
+              sizes="20vw"
+              src="/logo.gif"
+              className="inline-block mr-6"
+            />
+          ) : (
+            <Image
+              alt="Glider Logo"
+              height={20}
+              width={20}
+              sizes="20vw"
+              src="/logo.png"
+              className="inline-block mr-6"
+            />
+          )}
+        </Link>
         <NavLink href="/">Home</NavLink>
         <NavLink href="/about">About</NavLink>
         <NavLink href="/Subscribe">Contact</NavLink>
@@ -27,4 +62,4 @@ const Footer: React.FC = () => {
   );
 };
 
-export default Footer;
+export default Navbar;
