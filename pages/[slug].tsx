e65acf8606beead684a267ad31ebe3dcbed18e02
Swift <theycallmeswift@gmail.com>
2022-08-15 13:11:56 -0400

    Remove redundant image component

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,11 +1,13 @@
-import type { NextPage, GetStaticProps, GetStaticPaths } from "next";
 import type { Post } from "lib/types";
+import type { NextPage, GetStaticProps, GetStaticPaths } from "next";
+import type { ImageProps } from "next/future/image";
 
+import clsx from "clsx";
 import Link from "components/Link";
-import RoundedImage from "components/RoundedImage";
 import PostLayout from "layouts/PostLayout";
-import { sanityClient } from "lib/sanity";
 import imageMetadata from "lib/imageMetadata";
+import { sanityClient } from "lib/sanity";
+import NextImage from "next/future/image";
 import { MDXRemote } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
@@ -13,10 +15,18 @@ import rehypePrism from "rehype-prism-plus";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 
+const Image = ({
+  ...props
+}: React.ImgHTMLAttributes<HTMLImageElement> & ImageProps) => {
+  const rootClassName = clsx("w-full rounded-lg my-5 border border-gray-500");
+
+  return <NextImage className={rootClassName} {...props} />;
+};
+
 const Post: NextPage = ({ post }: { post: Post }) => {
   const mdxComponents = {
     a: Link,
-    img: RoundedImage,
+    img: Image,
   };
 
   return (
